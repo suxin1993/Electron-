601b7e0efe5b77a0dcc82c0d4062ec9900d0b350
suxin <subin@52tt.com>
2019-07-22 17:05:15 +0800

    [feat]:配置gulp,使得electron自动刷新

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+
+const gulp = require('gulp');
+const electron = require('electron-connect').server.create();
+ 
+gulp.task('watch:electron', function () {
+    electron.start();
+    gulp.watch(['./index.js'], electron.restart);
+    gulp.watch(['./ui/**/*.{js,css},./ui/index.html'], electron.reload);
+});
+
